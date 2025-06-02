0694f19704e7885344fcc2bc00f3d1c404923d96
fatimap-3 <fatpatel@algomau.ca>
2025-06-02 09:02:46 -0400

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-
-import HomePage from './Landing_Page/Home/HomePage';
-import AboutPage from './Landing_Page/About/AboutPage';
-import Pricing from './Landing_Page/Pricing/PricingPage';
-import ProductsPage from './Landing_Page/Products/ProductsPage';
-import SignUp from './Landing_Page/Signup/SignUp';
-import SupportPage from './Landing_Page/Support/SupportPage';
-import NavBar from './Landing_Page/NavBar';
-import Footer from './Landing_Page/Footer';
-import NotFound from './Landing_Page/NotFound';
-
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <BrowserRouter>
-       <NavBar/>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/pricing" element={<Pricing/>} />
-        <Route path="/products-page" element={<ProductsPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/support" element={<SupportPage />} />\
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
