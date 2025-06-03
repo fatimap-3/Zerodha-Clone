2e85a424d396fd96a32ac499b6333ac9488bb77c
fatimap-3 <fatpatel@algomau.ca>
2025-06-03 10:53:12 -0400

    Coded the Navigation Bar

--- src/Landing_Page/NavBar.js
+++ src/Landing_Page/NavBar.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 function NavBar() {
-    return (  
-        <h1>NavBar</h1>
+    return (
+            <nav class="navbar navbar-expand-lg bg-white border-bottom">
+                <div class="container p-2">
+                    <a class="navbar-brand" href='#'>
+                    <img src='Media/Images/logo.svg' alt='Logo' style={{width:"20%"}} /> </a>
+                    <button 
+                    class="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" a
+                    ria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <form class="navbar-nav" role="search">
+                        <ul class="navbar-nav mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Signup</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Products</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Support</a>
+                            </li>
+                        </ul>
+                        </form>
+                    </div>
+                    <div>
+                    <i className="fa fa-bars" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </nav>
     );
 }
 
