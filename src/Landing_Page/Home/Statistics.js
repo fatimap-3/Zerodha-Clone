e07d11fc2267194cd8d12985b630bb4579db669d
fatimap-3 <fatpatel@algomau.ca>
2025-06-11 20:19:42 -0400

    Added the links at the bottom of footer

--- src/Landing_Page/Home/Statistics.js
+++ src/Landing_Page/Home/Statistics.js
@@ -17,7 +17,7 @@ function Statistics() {
                 <p className='text-muted'>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
              </div>
             <div className='col-6 p-5'>
-                <img src='Media\Images\ecosystem.png' style={{width:"90%"}}/>
+                <img src='Media\Images\ecosystem.png' style={{width:"100%"}}/>
                 <div className="text-center">
             <a href="" className="mx-5" style={{ textDecoration: "none" }}>
               Explore our products{" "}
