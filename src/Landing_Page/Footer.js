a6e03277f65ca1062eb342b4c273104b6b1c4eb4
fatimap-3 <fatpatel@algomau.ca>
2025-06-11 20:19:42 -0400

    Added the links at the bottom of footer

--- src/Landing_Page/Footer.js
+++ src/Landing_Page/Footer.js
@@ -64,6 +64,17 @@ function Footer() {
             <p style={{color:'gray', fontSize:'10.4px'}}>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p>
             <p style={{color:'gray', fontSize:'10.4px'}}>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please <a href=''>create a ticket here.</a></p>
           </div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '20px', flexWrap: 'wrap', padding: '10px 0', fontSize: "13px"}}>
+            <a href='' style={{color:'gray', textDecoration:'none', fontWeight: "500"}}>NSE</a>
+                <a href='' style={{color:'gray', textDecoration:'none', fontWeight: "500"}}>BSE</a>
+                <a href='' style={{color:'gray', textDecoration:'none', fontWeight: "500"}}>MCX</a>
+                <a href='' style={{color:'gray', textDecoration:'none', fontWeight: "500"}}>Terms and Conditions</a>
+                <a href='' style={{color:'gray', textDecoration:'none', fontWeight: "500"}}>Policies and Procedures</a>
+                <a href='' style={{color:'gray', textDecoration:'none', fontWeight: "500"}}>Privacy Policy</a>
+                <a href='' style={{color:'gray', textDecoration:'none', fontWeight: "500"}}>Disclosure</a>
+                <a href='' style={{color:'gray', textDecoration:'none', fontWeight: "500"}}>For Investors attention</a>
+                <a href='' style={{color:'gray', textDecoration:'none', fontWeight: "500"}}>Investor Charter</a>
+          </div>
         </footer>
       );
 }
